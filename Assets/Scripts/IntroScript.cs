63770a2411fbd97cbb8458f58ead224d635a803d
Biter01 <sebastian.lamplmair@gmail.com>
2025-11-20 13:33:20 +0100

    Add Intro, Add boss sprite, Add Intro Sprites

---
+++ Assets/Scripts/IntroScript.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class IntroScript : MonoBehaviour
+{
+    public Image comicImage = null;         // Das UI-Image im Canvas
+    public Sprite[] panels;          // Deine Comic-Bilder in Reihenfolge
+    private int nextImgIdx = 0;
+
+    public float autoDelay = 5f;     // 0 = manuell per Klick; > 0 = automatisch
+    private float timer = 0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        comicImage.sprite = panels[0];
+        nextImgIdx++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.anyKeyDown || Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(SwitchPanelWithFade(panels[nextImgIdx]));
+            timer = 0f;
+
+        }
+
+        // Automatisch weiterschalten
+        if (autoDelay > 0)
+        {
+            timer += Time.deltaTime;
+            if (timer >= autoDelay)
+            {
+                if (panels.Length <= nextImgIdx)
+                {
+
+                    SceneManager.LoadScene("SampleScene");
+
+                } else
+                {
+                    StartCoroutine(SwitchPanelWithFade(panels[nextImgIdx]));
+                }
+                timer = 0f;
+            }
+            
+        }
+
+    }
+
+    IEnumerator SwitchPanelWithFade(Sprite nextSprite)
+    {
+        CanvasGroup cg = comicImage.GetComponent<CanvasGroup>();
+        if (cg == null) cg = comicImage.gameObject.AddComponent<CanvasGroup>();
+
+        float fadeSpeed = 2f;
+
+        // Fade out
+        for (float t = 1; t > 0; t -= Time.deltaTime * fadeSpeed)
+        {
+            cg.alpha = t;
+            yield return null;
+        }
+
+        // Sprite wechseln wenn ausgeblendet
+        comicImage.sprite = nextSprite;
+
+        // Fade in
+        for (float t = 0; t < 1; t += Time.deltaTime * fadeSpeed)
+        {
+            cg.alpha = t;
+            yield return null;
+        }
+
+        cg.alpha = 1;
+
+        nextImgIdx++;
+    }
+
+}
