3760f7a02cf2834af8d350accb033a3acc0e9583
felix <fkirchw@gmail.com>
2025-11-29 16:22:30 +0100

    caching descriptor id for faster lookup

--- Assets/Scripts/playerAni.cs
+++ Assets/Scripts/playerAni.cs
@@ -1,15 +1,15 @@
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
+    private static readonly int IsRunning = Animator.StringToHash("isRunning");
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 8f;
     [SerializeField] private Animator animator;
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb;
-    private bool isGrounded = true; // Zum Prüfen, ob Spieler auf dem Boden ist
+    private bool isGrounded = true; // Zum Prï¿½fen, ob Spieler auf dem Boden ist
 
     private void Awake()
     {   
@@ -29,20 +29,20 @@ public class Player : MonoBehaviour
             spriteRenderer.flipX = true;
         else if (horizontal > 0)
             spriteRenderer.flipX = false;
-
+        
         // Animation
-        animator.SetBool("isRunning", horizontal != 0);
+        animator.SetBool(IsRunning, horizontal != 0);
 
         // Sprung
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            isGrounded = false; // verhindert Doppelsprünge
+            isGrounded = false; // verhindert Doppelsprï¿½nge
             animator.speed = 0f;
         }
     }
 
-    // Prüfen, ob der Spieler wieder auf dem Boden ist
+    // Prï¿½fen, ob der Spieler wieder auf dem Boden ist
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
