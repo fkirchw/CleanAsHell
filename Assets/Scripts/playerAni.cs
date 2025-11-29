834f4288b6d30eabace0e0a0778f7c736f8a25b4
felix <fkirchw@gmail.com>
2025-11-29 17:15:26 +0100

    basic attack

--- Assets/Scripts/playerAni.cs
+++ Assets/Scripts/playerAni.cs
@@ -12,13 +12,17 @@ public class Player : MonoBehaviour
     private bool isGrounded = true; // Check if player is grounded
 
     private void Awake()
-    {   
+    {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
+        // TODO: Sweeping;
+        // TODO: Broom should only listen to collisions during hit animation -> should probably be script of broom
+        bool fire1Pressed = Input.GetButtonDown("Fire1");
+        bool isHitting = animator.GetCurrentAnimatorStateInfo(0).IsName("Hit");
         float horizontal = Input.GetAxisRaw("Horizontal");
 
         // horizontal movement
@@ -26,13 +30,16 @@ public class Player : MonoBehaviour
 
         // Filp direction
         if (horizontal < 0)
-            spriteRenderer.flipX = true;
+            transform.rotation = Quaternion.Euler(0, 180, 0);
         else if (horizontal > 0)
-            spriteRenderer.flipX = false;
+            transform.rotation = Quaternion.identity;
         
         // set animation
         animator.SetBool(IsRunning, horizontal != 0);
 
+        if (fire1Pressed && !isHitting)
+            animator.Play("Hit");
+        
         // jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
@@ -42,13 +49,13 @@ public class Player : MonoBehaviour
         }
     }
 
-    // Pr�fen, ob der Spieler wieder auf dem Boden ist
+    // Check if player has touched down
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true; // player has landed and can jump again
-            animator.speed = 1f;
-        }
+        // TODO: only count collisions from below
+        if (!collision.gameObject.CompareTag("Ground")) return;
+        
+        isGrounded = true; // player has landed and can jump again
+        animator.speed = 1f;
     }
 }
