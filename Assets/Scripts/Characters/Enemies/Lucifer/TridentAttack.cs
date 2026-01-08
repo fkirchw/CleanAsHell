d4809705ee594de066d84e76366b5bde223dab47
Biter01 <sebastian.lamplmair@gmail.com>
2026-01-08 20:54:31 +0100

    Added LuciferBossScene Added Lucifer Added Lucifer Animations Lucifer Sprites Lucifer Scripts

---
+++ Assets/Scripts/Characters/Enemies/Lucifer/TridentAttack.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using Characters.Enemies;
+using Characters.Enemies;
+using UnityEngine;
+
+public class AttackTrident : MonoBehaviour, IAttack
+{
+
+    [SerializeField] private float attackCooldown = 3f;
+    [SerializeField] private float attackDistance = 3f;
+    [SerializeField] private Vector2 knockbackDir = new Vector2(3,1);
+    [SerializeField] private float knockbackForce = 3f;
+    [SerializeField] private int damage = 5;
+    [SerializeField] private Vector2 damageRange = new Vector2(3, 1);
+
+    private bool canAttack = true;
+    private LuciferController controller;
+
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<LuciferController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!canAttack)
+        {
+            return;
+        }
+    }
+    public void Attack()
+    {
+        StartCoroutine(AttackRoutine());
+    }
+
+    public IEnumerator AttackCooldown()
+    {
+        canAttack = false;
+
+        // Hier kommt dein Angriffscode
+
+        // Zunge abfeuern, Animation triggern, Collider aktivieren, etc.
+
+        // Cooldown abwarten
+        yield return new WaitForSeconds(attackCooldown);
+
+        canAttack = true;
+    }
+
+    private IEnumerator AttackRoutine()
+    {
+        yield return new WaitForSeconds(0.5f); // 10 ms
+        controller.OnDamageDelt(this);
+    }
+
+    public Vector2 GetKnockbackDir() => knockbackDir;
+
+    public float GetKnockbackForce() => knockbackForce;
+
+
+    public float GetAttackCooldown() => attackCooldown;
+
+    public float GetAttackDistance() => attackDistance;
+
+    public string GetAttackName() => GetType().Name;
+
+    public bool GetCanAttack() => canAttack;
+
+    public int GetDamage() => damage;
+
+    Vector2 IAttack.GetDamageRange() => damageRange;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+}
