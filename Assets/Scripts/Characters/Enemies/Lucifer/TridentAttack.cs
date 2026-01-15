a6128f61cf9b9cf8a664e00858d3aa798019b616
Anatol <nicaanatol@gmail.com>
2026-01-16 01:05:25 +0200

    Trident

--- Assets/Scripts/Characters/Enemies/Lucifer/TridentAttack.cs
+++ Assets/Scripts/Characters/Enemies/Lucifer/TridentAttack.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class AttackTrident : MonoBehaviour, IAttack
 {
-    [SerializeField] private float attackCooldown = 4f;
+
+    [SerializeField] private float attackCooldown = 3f;
     [SerializeField] private float attackDistance = 3f;
     [SerializeField] private Vector2 knockbackDir = new Vector2(3,1);
     [SerializeField] private float knockbackForce = 3f;
     [SerializeField] private int damage = 5;
     [SerializeField] private Vector2 damageRange = new Vector2(3, 1);
-    [SerializeField] private float dashSpeed = 10f;
-    [SerializeField] private float dashDuration = 0.3f;
 
     private bool canAttack = true;
     private LuciferController controller;
-    private Rigidbody2D rb;
-    private SpriteRenderer spriteRenderer;
 
     Animator animator;
 
@@ -24,8 +21,6 @@ public class AttackTrident : MonoBehaviour, IAttack
     {
         animator = GetComponent<Animator>();
         controller = GetComponent<LuciferController>();
-        rb = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
     }
     
     public void Attack()
@@ -44,26 +39,14 @@ public class AttackTrident : MonoBehaviour, IAttack
 
     private IEnumerator AttackRoutine()
     {
-        float timer = 0f;
-        Vector2 dashDirection = spriteRenderer.flipX ? Vector2.left : Vector2.right;
-        
-        while (timer < dashDuration)
-        {
-            rb.linearVelocity = new Vector2(dashDirection.x * dashSpeed, 0);
-            timer += Time.deltaTime;
-            yield return null;
-        }
-        
-        rb.linearVelocity = Vector2.zero;
-        
-        yield return new WaitForSeconds(0.2f);
-        
+        yield return new WaitForSeconds(0.5f); // 10 ms
         controller.OnDamageDelt(this);
     }
 
     public Vector2 GetKnockbackDir() => knockbackDir;
 
     public float GetKnockbackForce() => knockbackForce;
+
 
     public float GetAttackCooldown() => attackCooldown;
 
@@ -76,4 +59,6 @@ public class AttackTrident : MonoBehaviour, IAttack
     public int GetDamage() => damage;
 
     Vector2 IAttack.GetDamageRange() => damageRange;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
 }
