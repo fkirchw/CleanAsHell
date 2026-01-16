4e9c6c6e42c8fb96c64907208ca96259a4e0b6be
Anatol <nicaanatol@gmail.com>
2026-01-16 22:22:49 +0200

    Statistics added

--- Assets/Scripts/PortalScript.cs
+++ Assets/Scripts/PortalScript.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PortalScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene("LuciferIntro");
-
+            LevelCompletedScript levelCompleted = FindObjectOfType<LevelCompletedScript>();
+            
+            if (levelCompleted != null)
+            {
+                levelCompleted.ShowLevelCompletedMenu();
+            }
         }
     }
 }
